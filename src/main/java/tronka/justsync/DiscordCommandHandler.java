aec9ac3b27f3517887717ab0e4ac3e6ff9bcc8e0
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2025-04-11 21:12:55 +0200

    feat: add alt list & improve commands

--- src/main/java/tronka/justsync/DiscordCommandHandler.java
+++ src/main/java/tronka/justsync/DiscordCommandHandler.java
@@ -3,6 +3,7 @@ package tronka.justsync;
 import com.mojang.authlib.GameProfile;
 
 import java.util.Objects;
+import java.security.Permissions;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -11,6 +12,7 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
@@ -28,15 +30,26 @@ public class DiscordCommandHandler extends ListenerAdapter {
         this.integration = integration;
         integration.getJda().updateCommands().queue();
         integration.getGuild().updateCommands().addCommands(
-            Commands.slash("link", "Link your minecraft with the code you got when joining")
-                .addOption(OptionType.STRING, "code", "Link code", true),
-            Commands.slash("linking", "Misc linking stuff").addSubcommands(
-                new SubcommandData("get", "Retrieve linking information").addOption(OptionType.USER, "user",
-                    "whose data to get").addOption(OptionType.STRING, "mc-name", "minecraft username to get data"),
-                new SubcommandData("unlink", "Unlink your account").addOption(OptionType.USER, "user", "user to unlink")
-                    .addOption(OptionType.STRING, "mc-name", "minecraft account to unlink")),
-            Commands.slash("list", "List the currently online players"),
-            Commands.slash("reload", "Reload the config file")).queue();
+                Commands.slash("link", "Link your minecraft with the code you got when joining")
+                        .addOption(OptionType.STRING, "code", "Link code", true),
+                Commands.slash("unlink", "Unlink your discord account from every linked minecraft account"),
+                Commands.slash("list", "List the currently online players"),
+
+                // restricted
+                Commands.slash("justsync", "Misc linking stuff").addSubcommands(
+                        new SubcommandData("get", "Retrieve linking information").addOption(OptionType.USER, "user",
+                                "whose data to get")
+                                .addOption(OptionType.STRING, "mc-name", "minecraft username to get data"),
+                        new SubcommandData("unlink", "Unlink your account")
+                                .addOption(OptionType.USER, "user", "user to unlink")
+                                .addOption(OptionType.STRING, "mc-name", "minecraft account to unlink"),
+                        new SubcommandData("alts", "shows all alts"))
+                        .setDefaultPermissions(
+                                DefaultMemberPermissions.enabledFor(
+                                        Permission.MODERATE_MEMBERS)),
+                Commands.slash("reload", "Reload the config file")
+                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)))
+                .queue();
     }
 
     @Override
@@ -45,37 +58,38 @@ public class DiscordCommandHandler extends ListenerAdapter {
             return;
         }
         switch (event.getName()) {
+
             case "link" -> {
                 String code = event.getOptions().getFirst().getAsString();
                 String message = this.integration.getLinkManager().confirmLink(event.getUser().getIdLong(), code);
                 event.reply(message).setEphemeral(true).queue();
             }
-            case "list" -> {
-                List<String> namesList = this.integration.getServer().getPlayerManager().getPlayerList().stream()
-                    .filter(player -> !this.integration.getVanishIntegration().isVanished(player))
-                    .map(player -> player.getName().getLiteralString())
-                    .map(Utils::escapeUnderscores)
-                    .toList();
 
-                String nameList = String.join(", ", namesList);
-                String message;
-
-                if (namesList.size() > 1) {
-                    message = "There are currently " + namesList.size() + " players online:";
-                } else if (namesList.size() == 1) {
-                    message = "There is currently " + namesList.size() + " player online:";
-                } else {
-                    message = "There are currently no players online";
-                }
-
-                event.reply(message + "\n" + nameList).setEphemeral(true).queue();
-            }
-            case "linking" -> linkingCommand(event);
-            case "reload" -> {
-                if (!PermissionUtil.checkPermission(event.getMember(), Permission.ADMINISTRATOR)) {
-                    event.reply("Insufficient permissions").setEphemeral(true).queue();
+            case "unlink" -> {
+                Optional<PlayerLink> dataOptional = this.integration.getLinkManager()
+                        .getDataOf(event.getMember().getIdLong());
+                if (dataOptional.isEmpty()) {
+                    event.reply(event.getMember().getAsMention() + " is not linked to any player")
+                            .setEphemeral(true)
+                            .queue();
                     return;
                 }
+
+                String message;
+                if (this.integration.getLinkManager().unlinkPlayer(event.getMember().getIdLong())) {
+                    message = "Successfully unlinked";
+                } else {
+                    message = "No linked player found";
+                }
+                event.reply(message).setEphemeral(true).queue();
+
+            }
+
+            case "list" -> listCommand(event);
+
+            case "justsync" -> linkingCommand(event);
+
+            case "reload" -> {
                 String result = this.integration.tryReloadConfig();
                 if (result.isEmpty()) {
                     result = "Reloaded config!";
@@ -85,11 +99,40 @@ public class DiscordCommandHandler extends ListenerAdapter {
         }
     }
 
+    private void listCommand(SlashCommandInteractionEvent event) {
+        List<String> namesList = this.integration.getServer().getPlayerManager().getPlayerList().stream()
+                .filter(player -> !this.integration.getVanishIntegration().isVanished(player))
+                .map(player -> player.getName().getLiteralString())
+                .map(Utils::escapeUnderscores)
+                .toList();
+
+        String nameList = String.join(", ", namesList);
+        String message;
+
+        if (namesList.size() > 1) {
+            message = "There are currently " + namesList.size() + " players online:";
+        } else if (namesList.size() == 1) {
+            message = "There is currently " + namesList.size() + " player online:";
+        } else {
+            message = "There are currently no players online";
+        }
+
+        event.reply(message + "\n" + nameList).setEphemeral(true).queue();
+
+    }
+
     private void linkingCommand(SlashCommandInteractionEvent event) {
         if (event.getMember() == null) {
             event.reply("Invalid Context").setEphemeral(true).queue();
             return;
         }
+
+        if (event.getSubcommandName().equals("alts")) {
+            listAlts(event);
+            return;
+        }
+
+
         Member target = event.getOption("user", OptionMapping::getAsMember);
         String minecraftName = event.getOption("mc-name", OptionMapping::getAsString);
 
@@ -114,7 +157,19 @@ public class DiscordCommandHandler extends ListenerAdapter {
         linkingWithMember(event, target);
     }
 
-    // run with checked permissions
+    private void listAlts(SlashCommandInteractionEvent event) {
+        List<PlayerLink> linksWithAlts = this.integration.getLinkManager().getAllLinks()
+                .filter(link -> link.altCount() > 0).toList();
+        String message = "";
+        for (PlayerLink link : linksWithAlts) {
+            List<String> alts = link.getAlts().stream().map(playerData -> playerData.getName()).toList();
+            String altsList = String.join("`, `", alts);
+            message = message + "`" + link.getPlayerName() + "`: `" + altsList + "`\n";
+        }
+        event.reply(message).setEphemeral(true).queue();
+    }
+
+	// run with checked permissions
     private void linkingWithMember(SlashCommandInteractionEvent event, Member target) {
         Optional<PlayerLink> dataOptional = this.integration.getLinkManager().getDataOf(target.getIdLong());
         if (dataOptional.isEmpty()) {
