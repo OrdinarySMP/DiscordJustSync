6111758e9468c5979a5efcbff9da4b455c94888e
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2026-09-01 22:29:10 +0200

    feat(integrations): do not relay messages when player is muted via banhammer

--- src/main/java/tronka/justsync/JustSyncApplication.java
+++ src/main/java/tronka/justsync/JustSyncApplication.java
@@ -25,6 +25,7 @@ import org.slf4j.LoggerFactory;
 import tronka.justsync.chat.ChatBridge;
 import tronka.justsync.chat.MinecraftToDiscordPreprocessor;
 import tronka.justsync.chat.RichPresenceUpdater;
+import tronka.justsync.compat.BanHammerIntegration;
 import tronka.justsync.compat.FloodgateIntegration;
 import tronka.justsync.compat.LuckPermsIntegration;
 import tronka.justsync.compat.VanishIntegration;
@@ -51,6 +52,7 @@ public class JustSyncApplication extends ListenerAdapter implements DedicatedSer
     private LuckPermsIntegration luckPermsIntegration;
     private VanishIntegration vanishIntegration;
     private FloodgateIntegration floodgateIntegration;
+    private BanHammerIntegration banHammerIntegration;
     private TimeoutManager timeoutManager;
     private DiscordLogger discordLogger;
     private RichPresenceUpdater richPresenceUpdater;
@@ -128,6 +130,7 @@ public class JustSyncApplication extends ListenerAdapter implements DedicatedSer
             this.luckPermsIntegration = new LuckPermsIntegration(this);
             this.vanishIntegration = new VanishIntegration(this);
             this.floodgateIntegration = new FloodgateIntegration(this);
+            this.banHammerIntegration = new BanHammerIntegration();
             this.jda.addEventListener(new DiscordEvents(this));
             this.richPresenceUpdater = new RichPresenceUpdater(this);
             this.minecraftToDiscordPreprocessor = new MinecraftToDiscordPreprocessor(this);
@@ -194,6 +197,10 @@ public class JustSyncApplication extends ListenerAdapter implements DedicatedSer
     }
 
     public FloodgateIntegration getFloodgateIntegration() { return this.floodgateIntegration;}
+
+    public BanHammerIntegration gBanHammerIntegration() {
+        return this.banHammerIntegration;
+    }
 
     public TimeoutManager getTimeoutManager() {
         return this.timeoutManager;
